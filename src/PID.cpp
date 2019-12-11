250c8465b42bd558020c23c1915061104e97b5b1
james94 <11840449+james94@users.noreply.github.com>
2019-12-11 14:12:31 -0800

    Implemented methods and constructor
    Implemented a parameter constructor that allows
    the user to specify the name of the device they
    want to control with the controller. To the
    Init() method, added more PID errors that needed
    to be initialized. Added code to the UpdateError()
    method to update PID errors based on CTE. Added
    code to TotalError() method that calculates the
    error for the steering or throttle device.
    TotalError() accounts for whether the user
    specified they are using the PID to control a
    steering or throttle device. UpdateSteering()
    and UpdateThrottle() methods were implemented
    to compute steering value and throttle value.
    These two methods are referenced in TotalError().

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,10 +1,20 @@
 #include "PID.h"
+#include <iostream>
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
+using std::cout;
+using std::endl;
 
 PID::PID() {}
+
+/**
+ * Sets name of device being controlled by PID Controller:
+ * 
+ * Device names: steering, throttle
+ */
+PID::PID(std::string control_name_)
+{
+  control_name = control_name_;
+}
 
 PID::~PID() {}
 
@@ -19,20 +29,96 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   Ki = Ki_;
   Kd = Kd_;
 
-  // Initialize integral crosstrack error
-  i_error = 0;
+  // Initialize PID crosstrack errors
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+  prev_error = 0.0;
 }
 
+
+/**
+ * UpdateError() function
+ * 
+ * Update PID errors based on cte.
+ */
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+  // Update proportional error
+  p_error = cte;
 
+  // Update differential error
+  d_error = p_error - prev_error;
+  prev_error = p_error;
+
+  // Update integral error
+  i_error += p_error;
 }
 
+/**
+ * TotalError() function
+ * 
+ * Calculate and return the total error for device being controlled:
+ *
+ * steering error or throttle error to control car's steering or
+ * speed is returned.
+ */
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  double total_error = 0.0;
+
+  if(control_name == "steering")
+  {
+    total_error = UpdateSteering();
+  }
+  else if(control_name == "throttle")
+  {
+    total_error = UpdateThrottle();
+  }
+  else
+  {
+    std::cout << "Name of device to be controlled unknown" << std::endl;
+  }
+  
+  return total_error; 
+}
+
+/**
+* UpdateSteering() function
+*
+* Calculates the steering value
+*/
+double PID::UpdateSteering() {
+  double steering_value = (-Kp * p_error) - (Kd * d_error) - (Ki * i_error);
+  
+  // Ensure the steering value is [-1, 1]
+  if(steering_value < -1)
+  {
+    steering_value = -1;
+  }
+  else if(steering_value > 1)
+  {
+    steering_value = 1;
+  }
+
+  return steering_value;
+}
+
+/**
+* UpdateThrottle() function
+*
+* Calculates the throttle value
+*/
+double PID::UpdateThrottle() {
+  double throttle_value = (Kp * p_error) + (Kd * d_error) + (Ki * i_error);
+  
+  // Ensure the throttle value is [0, 1]
+  if(throttle_value < 0)
+  {
+    throttle_value = 0;
+  }
+  else if(throttle_value > 1)
+  {
+    throttle_value = 1;
+  }
+
+  return throttle_value;
 }
